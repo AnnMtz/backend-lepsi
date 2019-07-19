12ae6d2974e6a07abd221338ab53f003054cd197
MonMtz <david@vkapp.mx>
2019-07-19 08:49:57 -0500

    últimos cambios en pacients model

--- src/models/emergencycontacts.js
+++ src/models/emergencycontacts.js
@@ -27,6 +27,12 @@ const schema = new Schema ({
         type: String,
         trim: true,
         minlength: 1
+    },
+    contactCelphone: {
+        required: true,
+        type: String,
+        trim: true,
+        minlength: 1
     }
 })
 
